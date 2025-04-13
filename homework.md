72addfd7b1e0ae63f1608b155cb92919a50638db
Shivkumar-7 <kumar00shiv00k123@gmail.com>
2025-04-13 08:19:40 +0530

    Learning Routing

--- homework.md
+++ homework.md
@@ -1,6 +1,14 @@
+--> First we run {npm init} on terminal this made configuration json file {package.json} that git command made for us {this file is just like index of our project like notebook has index meta data associated to project} {thus initialized node project}
+
+--> Now create src folder {any nomenclature} Inside it create app.js {starting point of application which is main JS file where write nodejs code} To run file {node src/app.js  what is nodejs program it just writing some piece of JS code nodejs take it and give to V8}
+
+--> Now for Backend of Devtinder we need to create server to listen users requests
+-->Now we will be using expressjs for creating server on nodejs application and run this application {project} 
+
 --> WE do not push nodemodules in github bcaz from package.json and package.json anytime by {npm i} by dependencies in them we can install nodemodules
 --> To tell git not track some folder changes make file on root of repo {.gitignore} now whatever path given inside it will ignore
 --> To initialize git repo run {git init}
 --> To commit changes {git add .} {{.} means all files on that changes area can also took one file {homework.md} are taken}
 --> Now {git commit -m "message"} Now I have commit all code in master {main branch} now want to push all code to github {remote repo as i am on local repo}
 --> Go to {github.com/new} Now want to push an existing repo from command line run codes {Happy All code on github Now}
+--> difference b/w package.json vs package-lock.json
--- src/app.js
+++ src/app.js
@@ -16,17 +16,21 @@ const app = express(); //instance of expressjs application {web server}
 
 //Now if I give route {"/test"} to app.use then server will on port 3000 but only handle request which has {/test in it}
 
-app.use((req, res) => { //this fnc is req handler
-     res.send("Hello from the server");
-});
+// app.use((req, res) => { //this fnc is req handler
+//      res.send("Hello from the server");
+// });
 
 app.use("/hello", (req, res) => {
      res.send("Hello Hello");
 });
 
-app.use("/", (req, res) => {
-     res.send("Hi I");
-});
+// "/hello" this handler will be like wilcard match prefix if any match with it then it will overwrite and other req handler not work.
+// also it match with only for "/hello/something" not "/hello123" as it will be different string
+//order matters
+
+// app.use("/", (req, res) => {
+//      res.send("Hi I");
+// });
 
 app.listen(3000, () => {
     console.log("I m Listening");
@@ -37,6 +41,40 @@ app.listen(3000, () => {
 //If install one time it will install on global level and work on every projects so done one time {installed on system level}
 //Now I have to only write {nodemon src/app.js} when run and it will automatically refresh server when code changes
 //Now I can add in package.json scripts "start" "dev" or anyname to {npm run start/dev} method for me to run code
+
+//-------------------------------- HTTP METHODS -------------------------------------------------------------------------------------------
+
+//When we type url on browser u are making GET {Request Method} API call to this route on server
+//Browser is worst way to test ur APIs / to test ur Routes
+//Use Postman for this {build, share APIs} In this we can made API calls just like browser
+//Make workspace/collections/test API call
+
+// app.use("/", (req, res) => {
+//      res.send("Hi I");
+// });
+//--> If I have use use method then it will handle all http methods {get, post, delete...} overwrite as order maintain
+//So make API req handler like this
+
+app.get("/user", (req, res) => {
+     res.send({firstName : "Shiv", lastname : "Thakur"});
+});
+
+app.post("/user", (req, res) => {
+     console.log("Shiv");
+     //saving data to DB
+   res.send("Data succesfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted Successfully!");
+});
+
+//Advance Routing Concepts
+//It will work for /ac, /abc, /ab
+app.get("/ab?c", (req, res) => {
+    res.send({firstName : "Shiv", lastName : "Kumar"});
+});
+
 
 
 
